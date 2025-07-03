1e2fa22a7492979995c4946c892aa33cab5c93a7
kaashntr <kaashntr@mail.com>
2025-07-04 00:15:55 +0300

    1

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name = "remote-back-rg"
-    storage_account_name = "remoteback"
+    storage_account_name = "remoteback1"
     container_name = "tfstate-container"
     key = "terraform.tfstate"
   }
