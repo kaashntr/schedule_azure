39e4db8a999ef4365b5fd294dd3f4325786fb990
kaashntr <kaashntr@mail.com>
2025-07-07 00:37:17 +0300

    1

--- terraform/export_subscription.sh
+++ terraform/export_subscription.sh
@@ -2,5 +2,5 @@
 export TF_VAR_az_subscription_id=$(az account show --query id --output tsv)
 # Add an echo to confirm it's working inside the script
 terraform $1
-sudo cp ../ansible/vars.yml /var/lib/jenkins/vars.yml
+cp ../ansible/vars.yml /var/lib/jenkins/vars.yml
 
